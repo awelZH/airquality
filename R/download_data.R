2cada403f68fde9076071f7f707fa8205bcf1ebd
Thomas Knecht <thomas.knecht@zh.ch>
2024-06-25 15:38:07 +0200

    add download functions

---
+++ R/download_data.R
@@ -0,0 +1,33 @@
+download_statpop_data <- function(year, destination_path){
+  
+  download_url <- get_bfs_metadata(year)
+  
+  download_zip(download_url, destination_path)
+}
+
+download_bafu_data <- function(id, destination_path){
+  
+  download_url <- get_swisstopo_metadata(id)$download_url
+  
+  
+  download_zip(download_url, destination_path)
+}
+
+
+
+download_zip <- function(download_url, destination_path){
+  
+  temp <- tempfile(tmpdir = destination_path, fileext = ".zip")
+  
+  command <- paste0("curl ", download_url, " --output ", temp)
+  system(command, intern = TRUE)
+  
+  # unzip
+  files <- unzip(temp, list = TRUE)
+  unzip(temp, exdir = destination_path)
+  
+  # delete temp zip
+  unlink(temp)
+  
+  
+}

40ab1321b9186b1d79d470218752132d1913200f
Thomas Knecht <thomas.knecht@zh.ch>
2024-07-17 15:27:02 +0200

    write own metadata functions

--- R/get_helpers.R
+++ R/get_helpers.R
@@ -1,104 +1,13 @@
 
-find_map_geolion <- function(wfs, version = "2.0.0", crs = 2056) {
-  
-  # query in R, see e.g. here: https://inbo.github.io/tutorials/tutorials/spatial_wfs_services/
-  # client <- ows4R::WFSClient$new(files$boundaries$wfs, serviceVersion = "2.0.0")
-  # client$getFeatureTypes(pretty = TRUE)
-  # client$getCapabilities()
-  # client$
-  #   getCapabilities()$
-  #   getOperationsMetadata()$
-  #   getOperations() %>%
-  #   purrr::map_chr(function(x){x$getName()})
-  # client$
-  #   describeFeatureType(typeName = "ms:grenzen") %>%
-  #   purrr::map_chr(function(x){x$getName()})
-  
-  url <- httr2::url_parse(wfs)
-  url$query <- list(service = "wfs",
-                    version = version,
-                    request = "GetFeature",
-                    typename = "ms:gem_grenzen", # "ms:gem_seen_grenzen",
-                    srsName = paste0("EPSG:", crs)
-  )
-  request <- httr2::url_build(url)
-  
-  return(request)
-}
 
 
-
-
-find_emikat_opendataswiss <- function(apiurl) {
-  
-  req <- httr2::request(apiurl)
-  req <- httr2::req_perform(req)
-  emikat <- httr2::resp_body_json(req)$result        
-  emikat <- unlist(purrr::map(emikat$resources, function(x) x$url))
-  emikat <- emikat[stringr::str_detect(emikat, ".csv")]
-  
-  return(emikat)
-}
-
-
-
-
-
-find_rsd_opendataswiss <- function(apiurl) {
-  
-  req <- httr2::request(apiurl)
-  req <- httr2::req_perform(req)
-  rsd <- httr2::resp_body_json(req)$result        
-  rsd <- unlist(purrr::map(rsd$resources, function(x) x$url))
-  rsd <- rsd[stringr::str_detect(rsd, ".csv")]
- 
-  return(rsd)
-}
-
-
-
-
-
-find_swisstopo_metadata <- function(id){
-
-  metadata_url <- paste0("https://data.geo.admin.ch/api/stac/v0.9/collections/",id,"/items")
-  
-  metadata <- rjson::fromJSON(file = metadata_url)
-  
-  features <- metadata$features[[1]]
-  
-  url <- features$assets[which(grepl("shp", features$assets))][[1]]$href
-  
-  # FIXME: wrong BAFU metadata => anpassen wenn mehrere Jahre vorhanden sind!
-  reference_year <- 2020 # as.Date(feature$properties$datetime)
-  swisstopo_metadata <- list(
-    url = url,
-    reference_year = reference_year 
-  )
-  
-  return(swisstopo_metadata)
-}
-
-
+## Macht es hier vielleicht Sinn, den download und lese-Schritt zu trennen?
+## am Schluss könnte man einen cleanup-Schritt machen umd die ganzen ordner wieder zu löschen 
 
 # courtesy of Statistikamt, modified
 get_bfs_statpop_rasterdata <- function(year, path_destination, boundary) {
   
-  # derive dataset url
-  bfs_nr <- paste0("ag-b-00.03-vz", year, "statpop")
-  meta_url <- gsub("bfs_nr", bfs_nr, "https://www.bfs.admin.ch/bfs/de/home/statistiken/kataloge-datenbanken.assetdetail.bfs_nr.html")
-  
-  command <- paste0("curl ", meta_url)
-  asset_page <- system(command, intern = TRUE)
-  
-  asset_page_total <- paste0(asset_page, collapse = " ")
-  
-  #asset_page <- RCurl::getURLContent(meta_url, .encoding = "latin1")
-  asset_number <- gsub(".*(https://.*assets/[0-9]+/).*", "\\1", asset_page_total)
-  asset_number <- gsub(".*/([0-9]+)/", "\\1", asset_number)
-  
-  download_url <- paste0("https://www.bfs.admin.ch/bfsstatic/dam/assets/",asset_number,"/master")
-  
+
   # download the ZIP file to a temporary location
   temp <- tempfile(tmpdir = path_destination, fileext = ".zip")
   command <- paste0("curl ", download_url, " --output ", temp)

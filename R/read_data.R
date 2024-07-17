67b6e34c3b0792a4b44e1a828313fd657ea264e0
Thomas Knecht <thomas.knecht@zh.ch>
2024-07-17 15:30:22 +0200

    add read functions

--- R/read_data.R
+++ R/read_data.R
@@ -1,4 +1,4 @@
-read_statpop_data <- function(year, destination_path, crs = 2056, boundary){
+read_statpop_raster_data <- function(year, destination_path, crs = 2056, boundary){
   
   file_to_read <- list.files(
     destination_path, 
@@ -31,4 +31,46 @@ read_statpop_data <- function(year, destination_path, crs = 2056, boundary){
   unlink(destination_path, recursive = TRUE)
   
   return(data_stars_zh)
+}
+
+
+read_bafu_raster_data <- function(destination_path, boundary){
+  
+  file_to_read <- list.files(
+    destination_path, 
+    pattern = "\\.shp$", 
+    full.names = TRUE,
+    recursive = TRUE
+  )
+
+  data_sf <- sf::read_sf(file_to_read)
+  
+  data_sf_zh <- dplyr::filter(
+    data_sf, 
+    sf::st_intersects(data, boundary, sparse = FALSE)
+  )
+  
+  return(data_sf_zh)
+}
+
+
+read_opendataswiss <- function(url, source){
+  read_url <- get_opendataswiss_metadata(url)
+
+  data <- purrr::map_df(read_url, function(x) readr::read_delim(x, delim = ","))
+
+  data <- dplyr::mutate(data, source = source)
+  
+  return(data)
+}
+
+
+read_local_csv <- function(file_name, encoding = "latin1"){
+  file <- paste0("inst/extdata/", file_name)
+  
+  locale <- readr::locale(encoding = encoding)
+  
+  data <- readr::read_delim(file, delim =";", locale = locale)
+  
+  return(data)
 }

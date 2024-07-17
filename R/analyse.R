662d16cdac0efb5b981f8e1af89b7cc1f43b513a
Thomas Knecht <thomas.knecht@zh.ch>
2024-07-17 15:28:44 +0200

    move to analyse helpers and prepare helpers

--- R/analyse.R
+++ R/analyse.R
@@ -78,92 +78,92 @@ exposition_distrib_cumulative <- function(data, y) {
 
 
 # ... only a valid approximation for vehicles weighting less than 3.5 t
-calc_vsp <- function(speed, accel, slope, # speed in m/s, accel in m/s/s, slope as ratio, mass = 3.5 in t
-                     vsp.a = 1.1, vsp.b = 0.132, vsp.c = 0.000302, vsp.g = 9.81) {
-  
-  vsp <- speed * (vsp.a * accel + (vsp.g * slope) + vsp.b) + (vsp.c * speed^3)
-  
-  return(vsp)
-}
+# calc_vsp <- function(speed, accel, slope, # speed in m/s, accel in m/s/s, slope as ratio, mass = 3.5 in t
+#                      vsp.a = 1.1, vsp.b = 0.132, vsp.c = 0.000302, vsp.g = 9.81) {
+#   
+#   vsp <- speed * (vsp.a * accel + (vsp.g * slope) + vsp.b) + (vsp.c * speed^3)
+#   
+#   return(vsp)
+# }
 
 
 
 
-calc_rsd_nox_emission <- function(NO, p, CO2, CO, HC) { # all concentrations in mixing ratios as percent
-  
-  Q <- CO / CO2
-  Q1 <- HC / CO2
-  Q2 <- NO / CO2
-  NO_emission <- 30 * Q2 * 860 / ((1 + Q + 6 * Q1) * 12)
-  NOx_emission <- NO_emission * 46 / (30 * (1 - p))
-  
-  return(NOx_emission)
-}
+# calc_rsd_nox_emission <- function(NO, p, CO2, CO, HC) { # all concentrations in mixing ratios as percent
+#   
+#   Q <- CO / CO2
+#   Q1 <- HC / CO2
+#   Q2 <- NO / CO2
+#   NO_emission <- 30 * Q2 * 860 / ((1 + Q + 6 * Q1) * 12)
+#   NOx_emission <- NO_emission * 46 / (30 * (1 - p))
+#   
+#   return(NOx_emission)
+# }
 
 
 
 
-# aggregate RSD data calculating: n, percentiles, median, mean, standard deviation, standard error
-aggregate_groups_rsd <- function(data, y, groups = c("vehicle_type", "vehicle_fuel_type", "vehicle_euronorm"),
-                                 nmin = 100, perc = list(ymin = 0.05, lower = 0.25, middle = 0.5, upper = 0.75, ymax = 0.95)) {
-  
-  data <-
-    data %>%
-    dplyr::group_by_at(dplyr::vars(groups)) %>%
-    dplyr::summarise(
-      n = length(na.omit(!!rlang::sym(y))),
-      min = quantile(!!rlang::sym(y), perc$ymin, na.rm = TRUE),
-      lower = quantile(!!rlang::sym(y), perc$lower, na.rm = TRUE),
-      middle = quantile(!!rlang::sym(y), perc$middle, na.rm = TRUE),
-      upper = quantile(!!rlang::sym(y), perc$upper, na.rm = TRUE),
-      max = quantile(!!rlang::sym(y), perc$ymax, na.rm = TRUE),
-      mean = mean(!!rlang::sym(y), na.rm = TRUE),
-      standarddeviation = sd(!!rlang::sym(y), na.rm = TRUE),
-      standarderror = standarddeviation / sqrt(n)
-    ) %>%
-    ungroup()
-  
-  data_all <-
-    data %>% 
-    dplyr::select(tidyr::all_of(groups)) %>% 
-    dplyr::distinct_all() %>% 
-    tidyr::expand(tidyr::crossing(!!!rlang::syms(groups)))
-  
-  data <- dplyr::left_join(data_all, data, by = groups) 
-  data <- dplyr::mutate_at(data, c("min", "lower", "middle", "upper", "max", "mean", "standarddeviation", "standarderror"), list(~ifelse(n < nmin, NA, .)))
-  data <- dplyr::mutate_at(data, c("min", "lower", "middle", "upper", "max", "mean", "standarddeviation", "standarderror"), list(~ifelse(is.nan(.), NA, .)))
-  data <- dplyr::mutate_at(data, c("min", "lower", "middle", "upper", "max", "mean", "standarddeviation", "standarderror"), list(~ifelse(is.infinite(.), NA, .)))
-  data$n <- ifelse(is.na(data$n), 0, data$n)
-  
-  return(data)
-}
-
-
-
-aggregate_nox_rsd <- function(data, meta, nmin = 50, groups = c("vehicle_type", "vehicle_fuel_type", "vehicle_euronorm")) {
-  
-  data <- 
-    data %>% 
-    aggregate_groups_rsd(y = "NOx_emission", groups = groups, nmin = nmin) %>% 
-    dplyr::rename(NOx_emission = mean) %>% 
-    dplyr::mutate(
-      unit = "g/kg fuel",
-      source = "Kanton Zürich/AWEL"
-    )
-  
-  if(is.null(meta)) {
-    data <- dplyr::select(data, !!c(groups, "NOx_emission", "unit", "n", "standarderror", "source"))
-  } else {
-    data <- 
-      data %>% 
-      dplyr::left_join(meta, by = groups) %>% 
-      dplyr::select(!!c(groups, "NOx_emission", "unit", "n", "standarderror", "NOx_emission_threshold_g_per_kg_fuel", "source"))
-  } 
-  
-  return(data)
-}
-
-
+# # aggregate RSD data calculating: n, percentiles, median, mean, standard deviation, standard error
+# aggregate_groups_rsd <- function(data, y, groups = c("vehicle_type", "vehicle_fuel_type", "vehicle_euronorm"),
+#                                  nmin = 100, perc = list(ymin = 0.05, lower = 0.25, middle = 0.5, upper = 0.75, ymax = 0.95)) {
+#   
+#   data <-
+#     data %>%
+#     dplyr::group_by_at(dplyr::vars(groups)) %>%
+#     dplyr::summarise(
+#       n = length(na.omit(!!rlang::sym(y))),
+#       min = quantile(!!rlang::sym(y), perc$ymin, na.rm = TRUE),
+#       lower = quantile(!!rlang::sym(y), perc$lower, na.rm = TRUE),
+#       middle = quantile(!!rlang::sym(y), perc$middle, na.rm = TRUE),
+#       upper = quantile(!!rlang::sym(y), perc$upper, na.rm = TRUE),
+#       max = quantile(!!rlang::sym(y), perc$ymax, na.rm = TRUE),
+#       mean = mean(!!rlang::sym(y), na.rm = TRUE),
+#       standarddeviation = sd(!!rlang::sym(y), na.rm = TRUE),
+#       standarderror = standarddeviation / sqrt(n)
+#     ) %>%
+#     ungroup()
+#   
+#   data_all <-
+#     data %>% 
+#     dplyr::select(tidyr::all_of(groups)) %>% 
+#     dplyr::distinct_all() %>% 
+#     tidyr::expand(tidyr::crossing(!!!rlang::syms(groups)))
+#   
+#   data <- dplyr::left_join(data_all, data, by = groups) 
+#   data <- dplyr::mutate_at(data, c("min", "lower", "middle", "upper", "max", "mean", "standarddeviation", "standarderror"), list(~ifelse(n < nmin, NA, .)))
+#   data <- dplyr::mutate_at(data, c("min", "lower", "middle", "upper", "max", "mean", "standarddeviation", "standarderror"), list(~ifelse(is.nan(.), NA, .)))
+#   data <- dplyr::mutate_at(data, c("min", "lower", "middle", "upper", "max", "mean", "standarddeviation", "standarderror"), list(~ifelse(is.infinite(.), NA, .)))
+#   data$n <- ifelse(is.na(data$n), 0, data$n)
+#   
+#   return(data)
+# }
+# 
+# 
+# 
+# aggregate_nox_rsd <- function(data, meta, nmin = 50, groups = c("vehicle_type", "vehicle_fuel_type", "vehicle_euronorm")) {
+#   
+#   data <- 
+#     data %>% 
+#     aggregate_groups_rsd(y = "NOx_emission", groups = groups, nmin = nmin) %>% 
+#     dplyr::rename(NOx_emission = mean) %>% 
+#     dplyr::mutate(
+#       unit = "g/kg fuel",
+#       source = "Kanton Zürich/AWEL"
+#     )
+#   
+#   if(is.null(meta)) {
+#     data <- dplyr::select(data, !!c(groups, "NOx_emission", "unit", "n", "standarderror", "source"))
+#   } else {
+#     data <- 
+#       data %>% 
+#       dplyr::left_join(meta, by = groups) %>% 
+#       dplyr::select(!!c(groups, "NOx_emission", "unit", "n", "standarderror", "NOx_emission_threshold_g_per_kg_fuel", "source"))
+#   } 
+#   
+#   return(data)
+# }
+# 
+# 
 
 
 
@@ -215,73 +215,73 @@ aggregate_nitrogen_deposition <- function(data) {
 
 
 
-aggregate_emissions <- function(data, fun = sum, groups = c("year", "pollutant", "unit", "sector", "subsector")) {
-  
-  data <-
-    data %>% 
-    dplyr::group_by_at(dplyr::vars(groups)) %>%
-    dplyr::summarise(emission = fun(emission)) %>% 
-    dplyr::ungroup() %>% 
-    dplyr::filter(emission > 0)
-  
-  return(data)
-}
+# aggregate_emissions <- function(data, fun = sum, groups = c("year", "pollutant", "unit", "sector", "subsector")) {
+#   
+#   data <-
+#     data %>% 
+#     dplyr::group_by_at(dplyr::vars(groups)) %>%
+#     dplyr::summarise(emission = fun(emission)) %>% 
+#     dplyr::ungroup() %>% 
+#     dplyr::filter(emission > 0)
+#   
+#   return(data)
+# }
 
 
 
-groups_emission_subsector <- function(data, threshold_fraction = 0.02, index = 1:3) {
-  
-  # mean emissions per category over all years
-  
-  groups <- aggregate_emissions(data, mean, c("pollutant", "sector", "subsector"))
-  
-  # recode subsectors with mean emissions less than the "big x" into subsector "sonstige"
-  
-  groups <- 
-    groups %>% 
-    dplyr::group_by(pollutant, sector) %>% 
-    dplyr::mutate(
-      subsector_new = dplyr::case_when(
-        emission < min(sort(emission, decreasing = TRUE)[index]) ~ "sonstige",
-        TRUE ~ subsector
-      ),
-      subsector_new = paste0(sector, " / ", subsector_new)
-    ) 
-  
-  # aggregate emissions accordingly
-  
-  groups2 <- aggregate_emissions(groups, sum, c("pollutant", "subsector_new"))
-  
-  # second iteration: if subsector emission < overall emission * threshold fraction, then also recode into "sonstige"
-  
-  groups2 <- 
-    groups2 %>% 
-    dplyr::group_by(pollutant) %>% 
-    dplyr::mutate(
-      subsector_new = dplyr::case_when(
-        subsector_new != "weitere" & emission < threshold_fraction * sum(emission)  ~ "sonstige",
-        TRUE ~ subsector_new
-      )
-    ) 
-  
-  # aggregate accordingly
-  
-  groups2 <- aggregate_emissions(groups, sum, c("pollutant", "subsector_new"))
-  
-  # restructure and prepare as lookup table for use outside this function
-  
-  groups2 <- dplyr::filter(groups2, !stringr::str_detect(subsector_new, "sonstige"))
-  
-  groups <-
-    groups %>% 
-    dplyr::select(-emission) %>% 
-    dplyr::left_join(groups2, by = c("pollutant", "subsector_new")) %>% 
-    dplyr::arrange(pollutant, sector, dplyr::desc(emission)) %>% 
-    dplyr::mutate(subsector_new = factor(subsector_new, levels = unique(.data$subsector_new))) %>% 
-    dplyr::select(-emission)
-  
-  return(groups)
-}
+# groups_emission_subsector <- function(data, threshold_fraction = 0.02, index = 1:3) {
+#   
+#   # mean emissions per category over all years
+# 
+#   groups <- aggregate_emissions(data, mean, c("pollutant", "sector", "subsector"))
+#   
+#   # recode subsectors with mean emissions less than the "big x" into subsector "sonstige"
+#   
+#   groups_test <- 
+#     groups %>% 
+#     dplyr::group_by(pollutant, sector) %>% 
+#     dplyr::mutate(
+#       subsector_new = dplyr::case_when(
+#         emission < min(sort(emission, decreasing = TRUE)[index]) ~ "sonstige",
+#         TRUE ~ subsector
+#       ),
+#       subsector_new = paste0(sector, " / ", subsector_new)
+#     ) 
+#   
+#   # aggregate emissions accordingly
+#   
+#   groups2 <- aggregate_emissions(groups, sum, c("pollutant", "subsector_new"))
+#   
+#   # second iteration: if subsector emission < overall emission * threshold fraction, then also recode into "sonstige"
+#   
+#   groups2 <- 
+#     groups2 %>% 
+#     dplyr::group_by(pollutant) %>% 
+#     dplyr::mutate(
+#       subsector_new = dplyr::case_when(
+#         subsector_new != "weitere" & emission < threshold_fraction * sum(emission)  ~ "sonstige",
+#         TRUE ~ subsector_new
+#       )
+#     ) 
+#   
+#   # aggregate accordingly
+#   
+#   groups2 <- aggregate_emissions(groups, sum, c("pollutant", "subsector_new"))
+#   
+#   # restructure and prepare as lookup table for use outside this function
+#   
+#   groups2 <- dplyr::filter(groups2, !stringr::str_detect(subsector_new, "sonstige"))
+#   
+#   groups <-
+#     groups %>% 
+#     dplyr::select(-emission) %>% 
+#     dplyr::left_join(groups2, by = c("pollutant", "subsector_new")) %>% 
+#     dplyr::arrange(pollutant, sector, dplyr::desc(emission)) %>% 
+#     dplyr::mutate(subsector_new = factor(subsector_new, levels = unique(.data$subsector_new))) %>% 
+#     dplyr::select(-emission)
+#   
+#   return(groups)
+# }
 
 
 
--- R/prepare.R
+++ R/prepare.R
@@ -37,38 +37,38 @@ restructure_rsd_for_vsp <- function(data) {
 }
 
 
-filter_rsd <- function(data, filters) {
+# filter_rsd <- function(data, filters) {
+# 
+#   data <- 
+#     data %>% 
+#     dplyr::filter(
+#       vehicle_model_year %in% filters$min[filters$parameter == "vehicleyears"]:filters$max[filters$parameter == "vehicleyears"] &
+#         (acceleration >= filters$min[filters$parameter == "accelerationrange"] & acceleration <= filters$max[filters$parameter == "accelerationrange"]) &
+#         (velocity >= filters$min[filters$parameter == "velocityrange"] & velocity <= filters$max[filters$parameter == "velocityrange"]) &
+#         (vehicle_specific_power >= filters$min[filters$parameter == "vsprange"] & vehicle_specific_power <= filters$max[filters$parameter == "vsprange"]) &
+#         vehicle_unloaded_weight <= filters$max[rsd_filters$parameter == "weightmax"] &
+#         !is.na(value)
+#     ) %>%
+#     tidyr::spread(parameter, value) %>%
+#     dplyr::filter(!is.na(NO + CO2 + CO + HC)) # all concentrations are nessecary for NOx emission calculation
+#   
+#   return(data)
+# }
 
-  data <- 
-    data %>% 
-    dplyr::filter(
-      vehicle_model_year %in% filters$min[filters$parameter == "vehicleyears"]:filters$max[filters$parameter == "vehicleyears"] &
-        (acceleration >= filters$min[filters$parameter == "accelerationrange"] & acceleration <= filters$max[filters$parameter == "accelerationrange"]) &
-        (velocity >= filters$min[filters$parameter == "velocityrange"] & velocity <= filters$max[filters$parameter == "velocityrange"]) &
-        (vehicle_specific_power >= filters$min[filters$parameter == "vsprange"] & vehicle_specific_power <= filters$max[filters$parameter == "vsprange"]) &
-        vehicle_unloaded_weight <= filters$max[rsd_filters$parameter == "weightmax"] &
-        !is.na(value)
-    ) %>%
-    tidyr::spread(parameter, value) %>%
-    dplyr::filter(!is.na(NO + CO2 + CO + HC)) # all concentrations are nessecary for NOx emission calculation
-  
-  return(data)
-}
 
-
-merge_restructure_rsd <- function(data, meta) {
-  
-  data <- 
-    data %>% 
-    dplyr::mutate(vehicle_euronorm = dplyr::recode(vehicle_euronorm, !!!c("Euro5a" = "Euro5", "Euro5b" = "Euro5"))) %>% # merge both sub-Euro5 norms since they are quite similar
-    dplyr::left_join(dplyr::filter(meta, is.na(as.numeric(vehicle_euronorm))), by = c("vehicle_type", "vehicle_fuel_type", "vehicle_euronorm")) %>% 
-    dplyr::mutate(
-      vehicle_type = factor(vehicle_type, levels = c("passenger car", "light duty vehicle")),
-      vehicle_fuel_type = factor(vehicle_fuel_type, levels = c("gasoline", "diesel"))
-    )
-  
-  return(data)
-}
+# merge_restructure_rsd <- function(data, meta) {
+#   
+#   data <- 
+#     data %>% 
+#     dplyr::mutate(vehicle_euronorm = dplyr::recode(vehicle_euronorm, !!!c("Euro5a" = "Euro5", "Euro5b" = "Euro5"))) %>% # merge both sub-Euro5 norms since they are quite similar
+#     dplyr::left_join(dplyr::filter(meta, is.na(as.numeric(vehicle_euronorm))), by = c("vehicle_type", "vehicle_fuel_type", "vehicle_euronorm")) %>% 
+#     dplyr::mutate(
+#       vehicle_type = factor(vehicle_type, levels = c("passenger car", "light duty vehicle")),
+#       vehicle_fuel_type = factor(vehicle_fuel_type, levels = c("gasoline", "diesel"))
+#     )
+#   
+#   return(data)
+# }
 
 
 

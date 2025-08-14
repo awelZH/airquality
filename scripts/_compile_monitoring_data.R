a165ede0a4590c20cbd109b41ab97a462208bd7a
Jörg Sintermann <joerg.sintermann@bd.zh.ch>
2025-08-14 10:58:55 +0200

    initialise airquality.data

--- scripts/_compile_monitoring_data.R
+++ scripts/_compile_monitoring_data.R
@@ -6,17 +6,17 @@
 # TODO: ... replace airquality.data as soon as useful api methods are available for this kind of data
 # ---
 data_monitoring_aq <- airquality.data::data_monitoring_aq
-data_monitoring_ndep <- airquality.data::data_monitoring_ndep
+# data_monitoring_ndep <- airquality.data::data_monitoring_ndep
 
 
 # aggregate dataset ...
 # ---
 # => aggregate individual components of nitrogen deposition
-data_monitoring_ndep <- aggregate_nitrogen_deposition(data_monitoring_ndep)
+# data_monitoring_ndep <- aggregate_nitrogen_deposition(data_monitoring_ndep)
 
 
 # write output datasets & clean up:
 # ---
 write_local_csv(data_monitoring_aq, file = "inst/extdata/output/data_airquality_monitoring_y1.csv")
-write_local_csv(data_monitoring_ndep, file = "inst/extdata/output/data_ndep_monitoring_y1.csv")
+# write_local_csv(data_monitoring_ndep, file = "inst/extdata/output/data_ndep_monitoring_y1.csv")
 rm(list = c("data_monitoring_aq", "data_monitoring_ndep"))
